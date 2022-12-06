c547eb0da7f49e1ae2df7a8182eb85ac8375ea0c
haku4130 <101144756+haku4130@users.noreply.github.com>
2022-12-06 17:59:28 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,9 +10,11 @@ int main() {
     Employee employee2;
     cout << department.add_employee(&employee2) << " " << department.add_employee(&employee) << " " << department.add_employee(&supervisor) << endl;
     cout << typeid(employee2).name() << " " << typeid(supervisor).name() << endl;
-    Employee *ptr = company.search(1);
+    Employee *ptr = company.search(2);
     company.get_table();
-    department.delete_employee(1);
+    department.delete_employee(2);
+    company.get_table();
+    company.emp_to_sup(&department, 1);
     company.get_table();
     return 0;
 }
