f2c70126b02c3aa959073ddebaaa0d3ec58460d1
haku4130 <101144756+haku4130@users.noreply.github.com>
2022-12-21 18:39:52 +0300

    Добавление коментов под doxygen

--- Human Resources Department.h
+++ Human Resources Department.h
@@ -11,6 +11,17 @@ using namespace std;
 class Supervisor;
 class Company;
 
+
+/**
+* \brief Информация о элементах класса Employee
+* @param name Имя
+* @param birth_year Год рождения
+* @param position Должность
+* @param education Образование
+* @param salary Зарплата
+*
+*/
+
 class Employee {
 protected:
     int id;
@@ -20,7 +31,7 @@ protected:
     string education{};
     int salary{};
     static int Count;
-public:
+public: ///В методах класса Employee находятся только get/set
     Employee(const string &name, int birth_year, const string &position, const string &education, int salary);
     Employee();
     [[nodiscard]] int get_id() const;
@@ -39,6 +50,8 @@ public:
     void set_salary(int new_salary);
 };
 
+
+
 class Company{
 public:
     class Department {
@@ -46,32 +59,35 @@ public:
         my_vector<Employee *> employees_array;
         string department_name{};
     public:
+        /**
+         * \brief Класс Department содержит в себе свое название и список сотрудников
+         */
         Department ();
         explicit Department(const string &);
         string get_department_name();
-        int add_employee(Employee *);
-        Employee *search(int id);
-        void delete_employee(int id);
+        int add_employee(Employee *); ///< Добавление работника в департамент
+        Employee *search(int id); ///< Поиск работника только в этом департаменте
+        void delete_employee(int id); ///< Удаление работника только из этого департамента
         size_t get_position(int id);
-        void get_table();
-        void free_department();
-        int emp_to_sup (int id);
+        void get_table(); ///< Вывод списка работников департамента в консоль
+        void free_department(); ///< Освобождение памяти департамента
+        int emp_to_sup (int id); ///< Преобразование обычного сотрудника в руководящего
         void set_department_name (const string &new_name);
     };
     Company ();
     explicit Company (const string &);
-    Department *search_dep_with_emp(int);
-    Employee *search (int);
-    Department *search (const string &);
-    Department *get_department (int i);
-    void add_department (Company::Department *);
-    void delete_employee(int id);
-    void delete_department (int i);
+    Department *search_dep_with_emp(int); ///< Поиск департамента по id сотрудника
+    Employee *search (int); ///< Поиск сотрудника в компании по всем департаментам
+    Department *search (const string &); ///< Поиск департамента по его названию
+    Department *get_department (int i); ///< Выбор департамента по индексу
+    void add_department (Company::Department *); ///< Добавление департамента в компанию
+    void delete_employee(int id); ///< Удаление работника из всей компании
+    void delete_department (int i); ///< Удаление департамента
     string get_company_name();
-    void get_table();
-    void get_departments ();
-    unsigned long get_dep_amount ();
-    void free_company();
+    void get_table(); ///< Вывод списка работников компании в консоль
+    void get_departments (); ///< Вывод списка департаментов в консоль
+    unsigned long get_dep_amount (); ///< Получить количество департаментов
+    void free_company(); ///< Освобождение памяти всей компании
     void set_company_name (const string &new_name);
 private:
     my_vector<Department *> departments;
@@ -85,7 +101,7 @@ public:
     Supervisor ();
     string get_type () override;
     Supervisor(const string &name, int birth_year, const string &position, const string &education, int salary);
-    void set_department_ptr (Company::Department *ptr);
+    void set_department_ptr (Company::Department *ptr); ///< Задать управляемый департамент
     Supervisor &operator = (Employee *);
 };
 
